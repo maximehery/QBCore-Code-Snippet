23321ba2e43c0e3c50e702f840bc59216fdb6499
GitHub Action <action@github.com>
2021-07-31 16:32:35 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/maximehery/qbcorecodesnippet/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.maximehery.qbcorecodesnippet.services
+
+import com.github.maximehery.qbcorecodesnippet.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
